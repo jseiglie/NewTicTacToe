628f4fe219b8aed9c87a37bfcc1261ff16ac3bbb
jseiglie <javierseiglie@gmail.com>
2022-02-02 10:46:27 +0000

    logic

--- src/js/component/Board.jsx
+++ src/js/component/Board.jsx
@@ -4,19 +4,42 @@ import O from "../component/O.jsx";
 import X from "../component/X.jsx";
 
 const Board = () => {
-	const [myBoard, setMyBoard] = useState([
+	const [table, setTable] = useState([
 		[undefined, undefined, undefined],
 		[undefined, undefined, undefined],
 		[undefined, undefined, undefined],
 	]);
+
+	const [turn, setTurn] = useState("x");
+
+	const CheckWinner = () => {
+		// * TODO: Comprobar si alguien gana
+	};
+
+	const ChangeTurn = () => setTurn(turn === "x" ? "o" : "x");
+
+	const SetValue = (i, j) => {
+		if (typeof table[i][j] === "undefined") {
+			const tmp = table;
+			tmp[i][j] = turn;
+			setTable(table);
+			ChangeTurn();
+			CheckWinner();
+		} else {
+			alert("Hey esta posicion ya estaba ocupada");
+		}
+	};
+
 	return (
 		<>
 			<table>
 				<tbody>
-					{myBoard.map((row) => (
-						<tr>
-							{row.map((column) => (
-								<td>{column}</td>
+					{table.map((row, i) => (
+						<tr key={i}>
+							{row.map((column, j) => (
+								<td onClick={() => SetValue(i, j)} key={j}>
+									<div className="item">{column}</div>
+								</td>
 							))}
 						</tr>
 					))}
