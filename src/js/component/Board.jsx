42e1b06d4a66828aa9e5538cbf15dd57d7cb87d5
jseiglie <javierseiglie@gmail.com>
2022-02-02 20:48:51 +0000

    diagonals

--- src/js/component/Board.jsx
+++ src/js/component/Board.jsx
@@ -12,56 +12,6 @@ const Board = () => {
 
 	const [turn, setTurn] = useState("x");
 
-	// const CheckMayorDiagonal = () => {
-	// 	let i = 1;
-	// 	let ctrl = false;
-
-	// 	while (ctrl && i < table.length) {
-	// 		if (table[i][i] != table[i - 1][i - 1]) {
-	// 			ctrl = true;
-	// 		} else {
-	// 			i += 1;
-	// 		}
-	// 	}
-	// 	return ctrl;
-	// };
-
-	// const CheckRows = () => {
-	// 	// comprobar las filas
-	// 	let ctrl = false;
-	// 	let i = 0;
-
-	// 	while (ctrl && i < table.length) {
-	// 		// * Los valores de los elementos
-	// 		console.log(i);
-	// 		let ctrl2 = false;
-	// 		let j = 1;
-
-	// 		while (ctrl2 && j < table[i].length) {
-	// 			if (table[i][j - 1] != table[i][j]) {
-	// 				ctrl2 = true;
-	// 			} else {
-	// 				j += 1;
-	// 			}
-	// 		}
-
-	// 		ctrl = ctrl2 ? true : false;
-	// 		if (!ctrl) {
-	// 			i += 1;
-	// 			console.log("!ctrl");
-	// 		}
-	// 	}
-	// 	return ctrl;
-	// };
-
-	const CheckColumnns = () => {};
-
-	// const CheckWinner = () => {
-	// 	if (CheckRows() || CheckMayorDiagonal() || CheckColumnns()) {
-	// 		alert("Hay un ganador");
-	// 	}
-	// };
-
 	const CheckRow = () => {
 		let i = 0;
 		let ctrl = true;
@@ -90,9 +40,60 @@ const Board = () => {
 		}
 		return !ctrl;
 	};
+	const CheckColumn = () => {
+		let i = 1;
+		let ctrl = true;
+
+		while (ctrl && i < table.length) {
+			console.log(`i value: ${i}`);
+			// recorrer columnas => buscar NO coincidencias
+			let j = 0;
+			let ctrl2 = true;
+			while (ctrl2 && j < table[i].length) {
+				if (
+					table[i][j] !== table[i - 1][j] ||
+					typeof table[i][j] === "undefined"
+				) {
+					ctrl2 = false;
+				} else {
+					j += 1;
+				}
+			}
+			ctrl = !ctrl2;
+			i += 1;
+		}
+		console.log(!ctrl);
+		if (!ctrl == true) {
+			alert("winner");
+		}
+		return !ctrl;
+	};
+
+	const CheckLeftDiagonal = () => {
+		if (
+			table[0][0] == table[1][1] &&
+			table[1][1] == table[2][2] &&
+			typeof table[1][1] !== "undefined"
+		) {
+			alert("Winner");
+		}
+	};
+
+	const CheckRightDiagonal = () => {
+		if (
+			table[0][2] == table[1][1] &&
+			table[1][1] == table[2][0] &&
+			typeof table[1][1] !== "undefined"
+		) {
+			alert("Winner");
+		}
+	};
 
 	const CheckWinner = () => {
 		CheckRow();
+		CheckColumn();
+		CheckLeftDiagonal();
+		CheckRightDiagonal();
 	};
 
 	const ChangeTurn = () => setTurn(turn === "x" ? "O" : "x");
